c87fe60f1e60c17bd0d3b780307567906e7d513c
rezmathers <rezmathers8@gmail.com>
2026-06-17 14:44:19 +0530

    fix: next config trailingSlash to fix 404s

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true,
   transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
   eslint: {
     ignoreDuringBuilds: true,
